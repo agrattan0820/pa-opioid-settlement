140023f351fa68eedd79672f6d20bd539d3191ac
Alexander Grattan <alex@grattan.me>
2024-05-29 21:14:25 -0400

    feat: add tooltip

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import * as Plot from "@observablehq/plot";
 import { csv } from "d3";
-
-console.log("hello!");
+import * as Plot from "@observablehq/plot";
 
 async function initChart() {
   const data = await csv("/pa-opiod-county-data.csv");
@@ -9,7 +7,21 @@ async function initChart() {
   const plot = Plot.plot({
     width: 800,
     marginLeft: 200,
-    marks: [Plot.barX(data, { y: "County", x: "Amount Left" })],
+    marks: [
+      Plot.barX(data, {
+        y: "County",
+        x: "Amount Left",
+        channels: {
+          "Amount Received": "Amount Received",
+          "Future Plans": "Future Plans",
+          "Non-Opioid Spending": "Non-Opioid Spending",
+          "Previous Amount": "Previous Amount",
+          "Remediation Committed": "Remediation Committed",
+          "Remediation Spent": "Remediation Spent",
+        },
+        tip: { fill: "#242424" },
+      }),
+    ],
   });
 
   console.log("DATA", data);
