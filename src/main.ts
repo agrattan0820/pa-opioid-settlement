96bb2bb29b7ea06c644542b1ac3b60fa2ff23aa7
Alexander Grattan <alex@grattan.me>
2024-05-29 20:56:08 -0400

    feat: initial commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import * as Plot from "@observablehq/plot";
+import { csv } from "d3";
+
+console.log("hello!");
+
+const data = await csv("/pa-opiod-county-data.csv");
+
+const plot = Plot.plot({
+  width: 800,
+  marginLeft: 200,
+  marks: [Plot.barX(data, { y: "County", x: "Amount Left" })],
+});
+
+console.log("DATA", data);
+
+const chart = document.querySelector("#chart");
+
+if (chart instanceof HTMLDivElement) {
+  chart.append(plot);
+}
